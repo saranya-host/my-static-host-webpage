4ad3d728a42fe2766c7d8d79e50eb2cb0338169e
Nithish <saranys2628@gmail.com>
2025-01-14 20:33:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // script.js
 function toggleDarkMode() {
     const darkModeStyle = document.getElementById('dark-mode-style');
+    const ambientLight = document.getElementById('ambient-light');
     if (darkModeStyle.disabled) {
         darkModeStyle.disabled = false;
+        ambientLight.style.display = 'block';
         localStorage.setItem('darkMode', 'enabled');
     } else {
         darkModeStyle.disabled = true;
+        ambientLight.style.display = 'none';
         localStorage.setItem('darkMode', 'disabled');
     }
 }
@@ -13,7 +16,11 @@ function toggleDarkMode() {
 // Check and apply dark mode on page load
 window.onload = () => {
     const darkMode = localStorage.getItem('darkMode');
+    const ambientLight = document.getElementById('ambient-light');
     if (darkMode === 'enabled') {
         document.getElementById('dark-mode-style').disabled = false;
+        ambientLight.style.display = 'block';
+    } else {
+        ambientLight.style.display = 'none';
     }
 };
